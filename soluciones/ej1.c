42c15740b7b8e34f8580e592bfabb810b0bc9a79
OseiDiaz <oseidiaz5@gmail.com>
2026-02-21 13:18:52 -0600

    Ejercicio01
    en este ejercicio el codigo imprime cada una de las dirreciones de memoria del arreglo

--- soluciones/ej1.c
+++ soluciones/ej1.c
@@ -2,5 +2,10 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 1
-    return 0;
+    int a[6];
+    for (int i = 0; i <= 6; i++)
+    {
+        printf("%p\n", &a[i]);
+    }
+return 0;
 }
