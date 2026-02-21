9c3bf28cdb495522d5d913a18d6629b1087cb222
OseiDiaz <oseidiaz5@gmail.com>
2026-02-21 13:34:53 -0600

    Ejercicio 11
    en este programa se responden las preguntas hechas en el ejercicio 11, en forma de comentario.

--- soluciones/ej11.c
+++ soluciones/ej11.c
@@ -2,5 +2,20 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 11
+    /*
+salida: 
+&a[0] = 1000
+&a[1] = 1008
+&a[2] = 1016
+
+1. el tamaño del tipo es de 8 bytes;
+
+2. el tipo posible es double, ya que el tamaño de un double es de 8 bytes.
+
+3.la formula usada es:  
+
+Dirección = base + (i * tamaño del tipo)
+
+*/
     return 0;
 }
