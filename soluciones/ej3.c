4d2ea5838dfc9d6fbd9287b7fcfe06cd62d1cb13
OseiDiaz <oseidiaz5@gmail.com>
2026-02-21 13:54:38 -0600

    Ejercicio03
    En este programa el codigo imprime todas las direcciones de memoria de todos los arreglos (a, b, c).

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -2,5 +2,15 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 3
+    int a[3];
+    int b[3];
+    int c[3];
+
+    for (int i = 0; i <= 3; i++)
+    {
+        printf("%p\n", &a[i]);
+        printf("%p\n", &a[i]);
+        printf("%p\n", &a[i]);
+        printf("\n");
     return 0;
 }
