f74916d54042fc57415ae2b05e8b1c930e4678e4
OseiDiaz <oseidiaz5@gmail.com>
2026-02-21 15:21:11 -0600

    Ejercicio08
    El codigo calcula el numero de columnas que tiene un arreglo y tambien el tamaño total del mismo.

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -1,6 +1,17 @@
 
 #include <stdio.h>
-int main(){
+#define Base 2000
+int main()
+{
     // Resolver ejercicio 8
+int i = 1, j = 2;
+int direccion = 2024;
+
+int C = (direccion - Base) / 4;
+int NumColmna = C / 2;
+printf("El numero de columna es: %d", NumColmna);
+
+int tamañoTotal = NumColmna * sizeof(int) * 4;
+printf("\nEl tamaño total de la matriz es: %d bytes", tamañoTotal);
     return 0;
 }
