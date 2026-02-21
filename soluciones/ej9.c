c526cf78f2af040143b21ae61ca33c94c45985cf
OseiDiaz <oseidiaz5@gmail.com>
2026-02-21 15:32:01 -0600

    Ejercicio09
    El codigo accede a la direccion [-1] de un arreglo y explica teoricamente por que esto es una mala practica y el por qué es que compila.

--- soluciones/ej9.c
+++ soluciones/ej9.c
@@ -2,5 +2,23 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 9
+    int a[5];
+printf("%p\n", &a[-1]);
+printf("%p\n", &a[5]);
+
+/*
+¿Por qué compila?
+
+	El compilador de C no suele realizar verificación de límites
+
+¿Por qué es peligroso?
+
+Porque se pueden acceder a posiciones de memoria que no pertenecen al arreglo
+lo que puede causar errores de ejecución o vulnerabilidades de seguridad.
+
+¿Qué revela sobre la memoria?
+
+Revela que la memoria se trata como un espacio contiguo y lineal
+*/
     return 0;
 }
