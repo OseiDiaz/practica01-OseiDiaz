d0b4b4c46771e973e0fb42f64c0f070216419a75
OseiDiaz <oseidiaz5@gmail.com>
2026-02-21 15:37:07 -0600

    Ejercicio10
    El ejercicio declara un arreglo gigante y explica el por que marca un error al querer compilar

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -2,5 +2,14 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 10
+    int gigante[10000000];
+
+/*
+el por que falla. es un error de segmentacion
+el programa esta tratando de acceder a una parte de la memoria que no le pertenece
+esto se debe a que el arreglo gigante es demasiado grande para ser almacenado en la pila, 
+lo que causa un desbordamiento de pila. para solucionar este problema, 
+se puede declarar el arreglo como una variable global
+*/
     return 0;
 }
