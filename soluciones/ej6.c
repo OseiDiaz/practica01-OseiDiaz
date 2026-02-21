02db95e665eb384855936694e1c572192644eaa9
OseiDiaz <oseidiaz5@gmail.com>
2026-02-21 14:37:39 -0600

    Ejercicio06
    el ejercicio imprime todas las direcciones de la matriz bidimencional.

--- soluciones/ej6.c
+++ soluciones/ej6.c
@@ -2,5 +2,17 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 6
+    int m[2][4]; 
+for (int i = 0; i < 2; i++)
+{
+	for (int j = 0; j < 4; j++)
+	{
+		printf("%p \n", &m[i][j]);
+		
+	}
+}
+//el primer termino de la matris representa las filas y el segundo las columnas.
+//la formula es: 
+//				direccion = base + (i * numero de columnas + j) * tamaño del tipo de dato
     return 0;
 }
