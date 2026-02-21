29439150ddb7de6e9ec4ff307a2f29740db80fc0
OseiDiaz <oseidiaz5@gmail.com>
2026-02-21 14:26:44 -0600

    Ejercicio05
    El codigo confirma que lo que dice el estudiante del ejercicio 5 es correcto.

--- soluciones/ej5.c
+++ soluciones/ej5.c
@@ -2,5 +2,12 @@
 #include <stdio.h>
 int main(){
     // Resolver ejercicio 5
+    int a[11];
+    a[0] = 1000;
+for (int i = 1; i <= 10; i++)
+{
+    a[i] = a[i - 1] + 1;
+	printf("a[%d] = %d\n", i, a[i]);
+}
     return 0;
 }
